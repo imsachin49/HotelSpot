8dc12d3f3b8c824ff8c55186548684c221f9119d
Sachin <kumaradity7125@gmail.com>
2023-06-03 01:51:21 +0530

    error

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,41 +1,32 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import searchReducer from './searchSlice';
-// import { userReducer } from 'react';
-// import { combineReducers } from '@reduxjs/toolkit';
-
-// const rootReducer = {
-//   search: searchReducer,
-//   user:userReducer
-// };
-
-// const store = configureStore({
-//   reducer: rootReducer,
-// });
-
-// export default store;
-
-
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import searchReducer from './searchSlice';
 import userReducer from './userSlice';
+import {persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
 
 const rootReducer = combineReducers({
   search: searchReducer,
   user: userReducer,
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
